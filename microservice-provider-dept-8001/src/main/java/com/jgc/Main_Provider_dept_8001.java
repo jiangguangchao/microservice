11ec48e9e1d8036ed785df0529c8527b3abe968a
jgc <jiangguangchao157@163.com>
2020-03-18 09:39:39 +0800

    将部门微服务注册到eureka注册中心

--- microservice-provider-dept-8001/src/main/java/com/jgc/Main_Provider_dept_8001.java
+++ microservice-provider-dept-8001/src/main/java/com/jgc/Main_Provider_dept_8001.java
@@ -3,6 +3,7 @@ package com.jgc;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @description:
@@ -11,6 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @MapperScan("com.jgc.dao")
 @SpringBootApplication
+@EnableEurekaClient
 public class Main_Provider_dept_8001 {
     public static void main(String[] args) {
         SpringApplication.run(Main_Provider_dept_8001.class,args);
